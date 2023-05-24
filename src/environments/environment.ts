94f34ce1601f55eff70060b3204efe70f17e2a50
mohammad ali baymani <97632262+buymoney1@users.noreply.github.com>
2023-05-24 10:16:13 +0330

    initial

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  application:
+  {
+    name: 'angular-ssr',
+    version: 'Angular 16.0.1',
+    bootstrap: 'Bootstrap 5.2.3',
+    fontawesome: 'Font Awesome 6.4.0',
+  }
+};
